d638f30b0d696d4e020e7f4d307420d3ed52dfb0
RAFI7x <islamrak045@gmail.com>
2025-11-21 03:01:42 +0600

    Update bd-sounds.js

--- api/bd-sounds.js
+++ api/bd-sounds.js
@@ -5,55 +5,56 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  const popularSounds = [
-    {
-      id: 1,
-      name: "Amar Sonar Bangla",
-      audio: "https://www.myinstants.com/media/sounds/amar-sonar-bangla-instrumental.mp3",
-      type: "national_anthem"
-    },
-    {
-      id: 2,
-      name: "Dhaka Police Siren", 
-      audio: "https://www.myinstants.com/media/sounds/police-siren-bangladesh.mp3",
-      type: "siren"
-    },
-    {
-      id: 3,
-      name: "Rickshaw Horn",
-      audio: "https://www.myinstants.com/media/sounds/rickshaw-horn-bd.mp3",
-      type: "vehicle"
-    },
-    {
-      id: 4,
-      name: "Bangladeshi Wedding DJ",
-      audio: "https://www.myinstants.com/media/sounds/bangladeshi-wedding-dhol.mp3",
-      type: "celebration"
-    },
-    {
-      id: 5,
-      name: "Vuter Gaan",
-      audio: "https://www.myinstants.com/media/sounds/vuter-gaan-traditional.mp3",
-      type: "folk"
-    },
-    {
-      id: 6, 
-      name: "Tiger Growl",
-      audio: "https://www.myinstants.com/media/sounds/bengal-tiger.mp3",
-      type: "animal"
-    },
-    {
-      id: 7,
-      name: "Cricket Cheer",
-      audio: "https://www.myinstants.com/media/sounds/bangladesh-cricket-crowd.mp3",
-      type: "sports"
-    }
-  ];
-  
-  res.json({
-    success: true,
-    country: "Bangladesh",
-    count: popularSounds.length,
-    sounds: popularSounds
-  });
+  // Handle OPTIONS request for CORS
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  try {
+    const popularSounds = [
+      {
+        id: 1,
+        name: "Amar Sonar Bangla Instrumental",
+        audio: "https://www.myinstants.com/media/sounds/amar-sonar-bangla-instrumental.mp3",
+        type: "national_anthem"
+      },
+      {
+        id: 2,
+        name: "Police Siren Bangladesh", 
+        audio: "https://www.myinstants.com/media/sounds/police-siren-bangladesh.mp3",
+        type: "siren"
+      },
+      {
+        id: 3,
+        name: "Rickshaw Horn BD",
+        audio: "https://www.myinstants.com/media/sounds/rickshaw-horn-bd.mp3",
+        type: "vehicle"
+      },
+      {
+        id: 4,
+        name: "Bangladeshi Wedding Dhol",
+        audio: "https://www.myinstants.com/media/sounds/bangladeshi-wedding-dhol.mp3",
+        type: "celebration"
+      },
+      {
+        id: 5,
+        name: "Vuter Gaan Traditional",
+        audio: "https://www.myinstants.com/media/sounds/vuter-gaan-traditional.mp3",
+        type: "folk"
+      }
+    ];
+    
+    res.json({
+      success: true,
+      country: "Bangladesh",
+      count: popularSounds.length,
+      sounds: popularSounds
+    });
+    
+  } catch (error) {
+    res.json({
+      success: false,
+      error: error.message
+    });
+  }
 };
